c59e38ce0ca114d8eddad5cda4caaf7f9972e30e
demo <nasima365banu@gmail.com>
2026-06-18 13:40:58 +0530

    Completed Mockito Exercise 1 - Mocking and Stubbing

---
+++ src/test/java/Exercise1/MyServiceTest.java
@@ -0,0 +1,21 @@
+package Exercise1;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class MyServiceTest {
+    @Test
+    public void Test(){
+        ExternalApi mockApi=mock(ExternalApi.class);
+        when(mockApi.getData()).thenReturn("Mock Data");
+
+        MyService service=new MyService(mockApi);
+        String result=service.fetchData();
+        assertEquals("Mock Data",result);
+
+
+    }
+}
